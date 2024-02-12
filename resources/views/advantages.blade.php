2a318529946ff7e6abaa610f98c0e21f588eb4da
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2024-02-12 16:49:05 +0200

    add advantages text

--- resources/views/advantages.blade.php
+++ resources/views/advantages.blade.php
@@ -1,7 +1,32 @@
 @extends("layouts/app")
 @section("title")
-    Наші переваги
+    {{__('messages.advantages')}}
 @endsection
 @section("content")
-	Наші переваги
+    <section class="section">
+        <div class="container"><br /><br /><br />
+            <h1>{{__('messages.advantages')}}</h1>
+            {{__('messages.dream')}}
+            {{__('messages.do-dream')}}
+            {{__('messages.advantages-lot')}}
+            {{__('messages.time')}}
+            {{__('messages.time-e')}}
+            {{__('messages.long-y')}}
+            {{__('messages.long-y-e')}}
+            {{__('messages.constr')}}
+            {{__('messages.constr-e')}}
+            {{__('messages.doing')}}
+            {{__('messages.doing-e')}}
+            {{__('messages.reability')}}
+            {{__('messages.reability-e')}}
+            {{__('messages.tele')}}
+            {{__('messages.tele-e')}}
+            {{__('messages.eco')}}
+            {{__('messages.eco-e')}}
+            {{__('messages.clear')}}
+            {{__('messages.clear-e')}}
+
+        </div>
+    </section>
+
 @endsection
