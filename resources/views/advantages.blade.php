c2f22b0d8063d3cff45e0fa7e997c3e0564644a5
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2024-02-13 15:44:24 +0200

    add advantages

--- resources/views/advantages.blade.php
+++ resources/views/advantages.blade.php
@@ -4,28 +4,46 @@
 @endsection
 @section("content")
     <section class="section">
-        <div class="container"><br /><br /><br />
-            <h1>{{__('messages.advantages')}}</h1>
-            {{__('messages.dream')}}
-            {{__('messages.do-dream')}}
-            {{__('messages.advantages-lot')}}
-            {{__('messages.time')}}
-            {{__('messages.time-e')}}
-            {{__('messages.long-y')}}
-            {{__('messages.long-y-e')}}
-            {{__('messages.constr')}}
-            {{__('messages.constr-e')}}
-            {{__('messages.doing')}}
-            {{__('messages.doing-e')}}
-            {{__('messages.reability')}}
-            {{__('messages.reability-e')}}
-            {{__('messages.tele')}}
-            {{__('messages.tele-e')}}
-            {{__('messages.eco')}}
-            {{__('messages.eco-e')}}
-            {{__('messages.clear')}}
-            {{__('messages.clear-e')}}
+        <div class="container">
 
+            <div class="advantage-card-title">
+                <h1 class="advantages-title">{{__('messages.advantages')}}</h1><br />
+                {{__('messages.dream')}}<br /><br />
+                {{__('messages.do-dream')}}<br /><br />
+                {{__('messages.advantages-lot')}}
+            </div>
+            <div class="advantage-card">
+                <h2 class="advantage-title">{{__('messages.time')}}</h2>
+                <span class="advantage-description">{{__('messages.time-e')}}</span>
+            </div>
+            <div class="advantage-card">
+                <h2 class="advantage-title">{{__('messages.long-y')}}</h2>
+                <span class="advantage-description">{{__('messages.long-y-e')}}</span>
+            </div>
+            <div class="advantage-card">
+                <h2 class="advantage-title">{{__('messages.constr')}}</h2>
+                <span class="advantage-description">{{__('messages.constr-e')}}</span>
+            </div>
+            <div class="advantage-card">
+                <h2 class="advantage-title">{{__('messages.doing')}}</h2>
+                <span class="advantage-description">{{__('messages.doing-e')}}</span>
+            </div>
+            <div class="advantage-card">
+                <h2 class="advantage-title">{{__('messages.reability')}}</h2>
+                <span class="advantage-description">{{__('messages.reability-e')}}</span>
+            </div>
+            <div class="advantage-card">
+                <h2 class="advantage-title">{{__('messages.tele')}}</h2>
+                <span class="advantage-description">{{__('messages.tele-e')}}</span>
+            </div>
+            <div class="advantage-card">
+                <h2 class="advantage-title">{{__('messages.eco')}}</h2>
+                <span class="advantage-description">{{__('messages.eco-e')}}</span>
+            </div>
+            <div class="advantage-card">
+                <h2 class="advantage-title">{{__('messages.clear')}}</h2>
+                <span class="advantage-description">{{__('messages.clear-e')}}</span>
+            </div>
         </div>
     </section>
 
